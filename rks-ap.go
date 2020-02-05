2b38e295aa10ad3e1a0f653396bf009513dcaa82
drkchiloll <sam.womack@entropicos.com>
2020-02-05 08:35:20 -0600

    updated GetAPs method to recursively retrieve all APs connected to the controller

--- rks-ap.go
+++ rks-ap.go
@@ -3,6 +3,7 @@ package rkssz
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
 )
 
 // RksAP ruckus ap properties
@@ -12,6 +13,7 @@ type RksAP struct {
 	ApGroupID string `json:"apGroupId"`
 	Serial    string `json:"serial"`
 	Name      string `json:"name"`
+	LanPorts  int    `json:"lanPortSize"`
 }
 
 // RksApRes ... GetAP Result
@@ -21,19 +23,53 @@ type RksApRes struct {
 }
 
 // GetAPs retrieves APs associated with the Controller
-func (c *Client) GetAPs(o RksOptions) (RksApRes, error) {
-	req, err := c.genGetReq("/aps")
+func (c *Client) GetAPs(o RksOptions) ([]RksAP, error) {
+	var getMore func(o RksOptions, r []RksAP) ([]RksAP, error)
+	getMore = func(o RksOptions, rksAps []RksAP) ([]RksAP, error) {
+		req, err := c.genGetReq("/aps")
+		if err != nil {
+			return rksAps, err
+		}
+		c.addQS(req, o)
+
+		res, err := c.http.Do(req)
+		if err != nil {
+			return rksAps, fmt.Errorf("failed to get resp: %v", err)
+		}
+		defer res.Body.Close()
+		var aps RksApRes
+		json.NewDecoder(res.Body).Decode(&aps)
+		for _, ap := range aps.List {
+			rksAps = append(rksAps, ap)
+		}
+		if aps.HasMore {
+			i := aps.FirstIndex + 100
+			o.Index = strconv.Itoa(i)
+			return getMore(o, rksAps)
+		}
+		return rksAps, nil
+	}
+	return getMore(o, []RksAP{})
+}
+
+// GetApGroups retrieves list of AP Group Names with IDs
+func (c *Client) GetApGroups(o RksOptions, zoneID string) ([]RksObject, error) {
+	if c.serviceTicket == "" {
+		return nil, fmt.Errorf(loginErr)
+	}
+	ep := fmt.Sprintf("/rkszones/%s/apgroups", zoneID)
+	req, err := c.genGetReq(ep)
 	if err != nil {
-		return RksApRes{}, err
+		return nil, err
 	}
 	c.addQS(req, o)
-
 	res, err := c.http.Do(req)
 	if err != nil {
-		return RksApRes{}, fmt.Errorf("failed to get resp: %v", err)
+		return nil, fmt.Errorf("failed to get resp: %v", err)
 	}
 	defer res.Body.Close()
-	var rksAps RksApRes
-	json.NewDecoder(res.Body).Decode(&rksAps)
-	return rksAps, nil
+	var grps RksCommonRes
+	json.NewDecoder(res.Body).Decode(&grps)
+	fmt.Println(grps.TotalCount)
+	return grps.List, nil
 }
